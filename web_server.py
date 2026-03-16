e4e0c16116935a8dcad8b561dfe4a0141d9f74d3
Nishant27203 <nishantkumar27203@gmail.com>
2026-03-16 20:07:25 +0530

    Fix port for Render deployment

--- web_server.py
+++ web_server.py
@@ -69,5 +69,8 @@ def _send_user_list():
 
 if __name__ == "__main__":
     # Run on configured host/port
-    socketio.run(app, host=WEB_HOST, port=WEB_PORT, debug=True, allow_unsafe_werkzeug=True)
+    import os
+
+port = int(os.environ.get("PORT", 5000))
+socketio.run(app, host="0.0.0.0", port=port, debug=True, allow_unsafe_werkzeug=True)
 
